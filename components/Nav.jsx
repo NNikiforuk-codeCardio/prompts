c5de8e9c548b10746ea1c1f440bb641edd8d6c6c
Natalia <natnikiforuk@gmail.com>
2023-06-29 19:44:01 +0200

    Auth created

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,7 @@ import { signIn, signOut, getProviders } from "next-auth/react";
 const Nav = () => {
 	const isUserLoggedIn = true;
 	const [providers, setProviders] = useState(null);
+	const [toggleDropdown, setToggleDropdown] = useState(false);
 
 	useEffect(() => {
 		const setProviders = async () => {
@@ -52,6 +53,62 @@ const Nav = () => {
 								className="rounded-full"
 							/>
 						</Link>
+					</div>
+				) : (
+					<>
+						{providers &&
+							Object.values(providers).map((provider) => (
+								<button
+									key={provider.name}
+									onClick={() => signIn(provider.id)}
+									className="black_btn"
+								>
+									Sign In
+								</button>
+							))}
+					</>
+				)}
+			</div>
+
+			{/* Mobile Navigation */}
+			<div className="sm:hidden flex relative">
+				{isUserLoggedIn ? (
+					<div className="flex">
+						<Image
+							src="/assets/images/logo.svg"
+							width={37}
+							height={37}
+							alt="profile"
+							className="rounded-full"
+							onClick={() => setToggleDropdown((prev) => !prev)}
+						/>
+						{toggleDropdown && (
+							<div className="dropdown">
+								<Link
+									href="/profile"
+									className="dropdown_link"
+									onClick={() => setToggleDropdown(false)}
+								>
+									My Profile
+								</Link>
+								<Link
+									href="/create-prompt"
+									className="dropdown_link"
+									onClick={() => setToggleDropdown(false)}
+								>
+									Create Prompt
+								</Link>
+								<button
+									onClick={() => {
+										setToggleDropdown(false);
+										signOut();
+									}}
+									className="mt-5 w-full black_btn"
+								>
+									Sign Out
+								</button>
+							</div>
+						)}
 					</div>
 				) : (
 					<>
